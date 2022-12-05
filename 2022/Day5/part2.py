7f71cee3c9e888c45223e164bbd5c495a671aab5
Anand Rajaram <anandrajaram21@gmail.com>
2022-12-05 16:52:33 +0530

    feat: added day 5 solutions

---
+++ 2022/Day5/part2.py
@@ -0,0 +1,63 @@
+import string
+
+def push(stack, item):
+    stack.append(item)
+
+def pop(stack):
+    return stack.pop()
+
+def peek(stack):
+    return stack[-1]
+
+def move(num, from_stack, to_stack):
+    to_stack.extend(from_stack[-num:])
+    for i in range(num):
+        pop(from_stack)
+
+def split_line(line):
+    # Split the line at every 4 characters
+    return [line[i] for i in range(1, len(line), 4)]
+
+with open("input.txt") as f:
+    contents = f.readlines()
+    stackNum = 0
+    stacks = []
+    for i in range(len(contents)):
+        if "1" in contents[i]:
+            stacks = [[] for j in range(i+1)]
+            stackNum = i
+            break
+
+    lines = [split_line(line) for line in contents[:stackNum]]
+    lines = lines[::-1]
+
+    for i in range(len(lines)):
+        for j in range(len(lines[i])):
+            if lines[i][j] == " ":
+                continue
+            push(stacks[j], lines[i][j])
+    
+    instruction_lines = contents[stackNum+2:]
+    instruction_lines = [line.strip() for line in instruction_lines]
+
+    for i in range(len(instruction_lines)):
+        instruction = instruction_lines[i]
+        instruction = instruction.replace("move ", "")
+        f_in = instruction.find("from")
+        num = int(instruction[:f_in])
+        instruction = instruction[f_in:]
+        instruction = instruction.replace("from ", "")
+        f_to = instruction.find("to")
+        from_stack = int(instruction[:f_to])
+        to_stack = int(instruction[f_to+3:])
+
+        instruction_lines[i] = (num, from_stack, to_stack)
+    
+    for num, from_stack, to_stack in instruction_lines:
+        move(num, stacks[from_stack-1], stacks[to_stack-1])
+
+    final_string = ""
+    for i in stacks:
+        final_string += peek(i)
+
+    print(final_string)
